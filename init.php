91963275fe157e2986d07c5994208098a25d98b6
colab_01.diem <runtim3.error@gmail.com>
2012-06-01 18:43:57 -0600

    ...

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php 
+if (!isset($_SESSION))session_start();
+
+//APP_PATH
+
+//incluir modelo principal
+
+//incluir controlador principal
+
+//incluir vista principal
+require_once('composite_view/vista.php');
+require_once('composite_view/pagina.php');
+require_once('modelo.php');
+require_once('../vistas/theme1/layout_view.php');
+?>
